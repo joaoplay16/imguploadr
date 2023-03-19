ccb32f8193ac43bd7ea86bf5f70d9d36a67f11d9
João Pedro <joaoplay16@gmail.com>
2023-03-19 20:27:20 -0300

    create image view-model sample

--- controllers/image.js
+++ controllers/image.js
@@ -1,6 +1,36 @@
 module.exports = {
   index: function (req, res) {
-    res.render('image')
+    var viewModel = {
+      image: {
+        uniqueId: 1,
+        title: "Sample Image 1",
+        description: "",
+        filename: "sample1.jpg",
+        views: 0,
+        likes: 0,
+        timestamp: Date.now,
+      },
+      comments: [
+        {
+          image_id: 1,
+          email: "test@testing.com",
+          name: "Test Tester",
+          gravatar: "http://lorempixel.com/75/75/animals/1",
+          comment: "This is a test comment...",
+          timestamp: Date.now(),
+        },
+        {
+          image_id: 1,
+          email: "test@testing.com",
+          name: "Test Tester",
+          gravatar: "http://lorempixel.com/75/75/animals/2",
+          comment: "Another followup comment!",
+          timestamp: Date.now(),
+        },
+      ],
+    }
+
+    res.render("image", viewModel)
   },
   create: function (req, res) {
     res.send("The image:create POST controller")
