8d0158f9098d701dd831bce631067d05aeb94233
João Pedro <joaoplay16@gmail.com>
2023-03-23 23:04:57 -0300

    set up mongoose

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var express = require('express')
-
-config = require('./server/configure')
+var express = require("express"),
+  config = require("./server/configure"),
+  mongoose = require("mongoose")
 app = express()
-app.set('port', process.env.PORT || 3300)
-app.set('views', __dirname + '/views')
+app.set("port", process.env.PORT || 3300)
+app.set("views", __dirname + "/views")
 app = config(app)
 
-app.get('/', function(req, res){
-  res.send('Hello World')
+mongoose.connect("mongodb://localhost/imgPloadr")
+mongoose.connection.on("open", function () {
+  console.log("Mongoose connected.")
 })
 
-app.listen(app.get('port'), function(){
-  console.log('Server up: http://localhost:' + app.get('port'))
+app.listen(app.get("port"), function () {
+  console.log("Server up: http://localhost:" + app.get("port"))
 })
