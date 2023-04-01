36422c1e686abf6b17dd21946acaa2fcc35a3356
João Pedro <joaoplay16@gmail.com>
2023-03-31 21:18:39 -0300

    create delete image controller

--- controllers/image.js
+++ controllers/image.js
@@ -2,22 +2,22 @@ var fs = require("fs"),
   path = require("path"),
   sidebar = require("../helpers/sidebar"),
   Models = require("../models"),
-  md5 = require("md5")
+  md5 = require("md5");
 
 module.exports = {
   index: function (req, res) {
     var viewModel = {
       image: {},
       comments: [],
-    }
+    };
 
     Models.Image.findOne({ filename: { $regex: req.params.image_id } })
       .exec()
       .then((image) => {
         if (image) {
-          image.views = image.views + 1
-          viewModel.image = image
-          image.save()
+          image.views = image.views + 1;
+          viewModel.image = image;
+          image.save();
 
           Models.Comment.find(
             { image_id: image._id },
@@ -30,30 +30,30 @@ module.exports = {
           )
             .exec()
             .then((comments) => {
-              viewModel.comments = comments
+              viewModel.comments = comments;
 
               sidebar(viewModel, function (viewModel) {
-                res.render("image", viewModel)
-              })
+                res.render("image", viewModel);
+              });
             })
             .catch((err) => {
-              throw err
-            })
+              throw err;
+            });
         } else {
-          res.redirect("/")
+          res.redirect("/");
         }
       })
       .catch((err) => {
-        throw err
-      })
+        throw err;
+      });
   },
   create: function (req, res) {
     var saveImage = function () {
       var possible = "abcdefghijklmnopqrstuvwxyz0123456789",
-        imgUrl = ""
+        imgUrl = "";
 
       for (var i = 0; i < 6; i += 1) {
-        imgUrl += possible.charAt(Math.floor(Math.random() * possible.length))
+        imgUrl += possible.charAt(Math.floor(Math.random() * possible.length));
       }
 
       Models.Image.find({ filename: imgUrl })
@@ -61,11 +61,11 @@ module.exports = {
         .then((images) => {
           if (images.length > 0) {
             // if a matching image was found, try again (start over):
-            saveImage()
+            saveImage();
           } else {
             var tempPath = req.file.path,
               ext = path.extname(req.file.originalname).toLowerCase(),
-              targetPath = path.resolve("./public/upload/" + imgUrl + ext)
+              targetPath = path.resolve("./public/upload/" + imgUrl + ext);
 
             if (
               ext === ".png" ||
@@ -74,63 +74,94 @@ module.exports = {
               ext === ".gif"
             ) {
               fs.rename(tempPath, targetPath, function (err) {
-                if (err) throw err
+                if (err) throw err;
                 var newImg = new Models.Image({
                   title: req.body.title,
                   filename: imgUrl + ext,
                   description: req.body.description,
-                })
+                });
                 newImg.save().then((image) => {
-                  console.log("IMAGE RETRIEVED", image)
-                  res.redirect("/images/" + image.uniqueId)
-                })
-              })
+                  console.log("IMAGE RETRIEVED", image);
+                  res.redirect("/images/" + image.uniqueId);
+                });
+              });
             } else {
               fs.unlink(tempPath, function () {
-                res.json(500, { error: "Only image files are allowed." })
-              })
+                res.json(500, { error: "Only image files are allowed." });
+              });
             }
           }
         })
         .catch((err) => {
-          throw err
-        })
-    }
-    saveImage()
+          throw err;
+        });
+    };
+    saveImage();
   },
   like: function (req, res) {
-    Models.Image.findOne({ filename: { $regex: req.params.image_id }})
-    .then(image => {
-      if(image){
-        image.likes = image.likes +1
-        image.save().then(() => {
-          res.json({likes: image.likes})
-        }).catch(err => {
-          res.json(err)
-        })
+    Models.Image.findOne({ filename: { $regex: req.params.image_id } }).then(
+      (image) => {
+        if (image) {
+          image.likes = image.likes + 1;
+          image
+            .save()
+            .then(() => {
+              res.json({ likes: image.likes });
+            })
+            .catch((err) => {
+              res.json(err);
+            });
+        }
       }
-    })
+    );
   },
   comment: function (req, res) {
-    Models.Image.findOne({ filename: { $regex: req.params.image_id } })
-    .then(image => {
+    Models.Image.findOne({ filename: { $regex: req.params.image_id } }).then(
+      (image) => {
         if (image) {
-          var newComment = new Models.Comment(req.body)
-          newComment.gravatar = md5(newComment.email)
-          newComment.image_id = image._id
+          var newComment = new Models.Comment(req.body);
+          newComment.gravatar = md5(newComment.email);
+          newComment.image_id = image._id;
 
           newComment
             .save()
             .then((comment) => {
-              res.redirect(`/images/${image.uniqueId}#${comment._id}`)
+              res.redirect(`/images/${image.uniqueId}#${comment._id}`);
             })
             .catch((err) => {
-              res.json(err)
-            })
+              res.json(err);
+            });
         } else {
-          res.redirect("/")
+          res.redirect("/");
         }
       }
-    )
+    );
   },
-}
+  remove: function (req, res) {
+    Models.Image.findOne({ filename: { $regex: req.params.image_id } })
+    .exec()  
+    .then((image) => {
+        fs.unlink(
+          path.resolve(`./public/upload/${image.filename}`, function (err) {
+            if (err) {
+              throw err
+            }
+
+            Models.Comment.deleteMany({ image_id: image._id })
+            .exec()
+            .then(() => {
+              image.deleteOne().exec().then(() => {
+                res.json(true)
+              })
+            })
+            .catch(err => {
+              res.json(false)
+            })
+          })
+        );
+      })
+      .catch((err) => {
+        throw err;
+      });
+  },
+};
