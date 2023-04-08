82fad2f5b2154b3dad8f98d840deb39f69742c17
João Pedro <joaoplay16@gmail.com>
2023-04-07 23:19:40 -0300

    add the remaining tests. need refactoring to promise aproach

--- tests/controllers/image.test.js
+++ tests/controllers/image.test.js
@@ -40,8 +40,6 @@ describe("Image Controller", function () {
       likes: 0,
       save: sinon.spy(),
     }
-
-  
   })
   describe("Index", function () {
     it("should be defined", function () {
@@ -53,9 +51,46 @@ describe("Image Controller", function () {
     })
     it("should find Image by parameter id", function () {
       image.index(req, res)
-      expect(ModelsStub.Image.findOne).to.be.calledWith(
-        { filename: { $regex: "testing" } }
-      )
+      expect(ModelsStub.Image.findOne).to.be.calledWith({
+        filename: { $regex: "testing" },
+      })
     })
+    // describe("with found image model", function () {
+    //   beforeEach(function () {
+    //     ModelsStub.Image.findOne = sinon
+    //       .stub()
+    //       .returns({ exec: () => sinon.promise().resolve( testImage ) })
+    //   })
+    //   it("should incremement views by 1 and save", function () {
+    //     image.index(req, res)
+    //     expect(testImage.views).to.equal(1)
+    //     expect(testImage.save).to.be.called
+    //   })
+    //   it("should find related comments", function () {
+    //     image.index(req, res)
+    //     expect(ModelsStub.Comment.find).to.be.calledWith(
+    //       { image_id: 1 },
+    //       {},
+    //       { sort: { timestamp: 1 } }
+    //     )
+    //   })
+      // it("should execute sidebar", function () {
+      //   ModelsStub.Comment.find = sinon.stub().callsArgWith(3, null, [1, 2, 3])
+      //   image.index(req, res)
+      //   expect(sidebarStub).to.be.calledWith(
+      //     { image: testImage, comments: [1, 2, 3] },
+      //     sinon.match.func
+      //   )
+      // })
+      // it("should render image template with image and comments", function () {
+      //   ModelsStub.Comment.find = sinon.stub().callsArgWith(3, null, [1, 2, 3])
+      //   sidebarStub.callsArgWith(1, { image: testImage, comments: [1, 2, 3] })
+      //   image.index(req, res)
+      //   expect(res.render).to.be.calledWith("image", {
+      //     image: testImage,
+      //     comments: [1, 2, 3],
+      //   })
+      // })
+    // })
   })
 })
