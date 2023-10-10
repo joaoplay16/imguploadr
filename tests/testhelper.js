c8de4c17452339f9890755a56611e3216cdd1274
João Pedro <joaoplay16@gmail.com>
2023-10-09 22:40:01 -0300

    Use "sinon-chai"to make possible do more expressive assertions

--- tests/testhelper.js
+++ tests/testhelper.js
@@ -1,6 +1,6 @@
 var chai = require("chai"),
   sinon = require("sinon"),
   sinonChai = require("sinon-chai")
+chai.use(sinonChai)
 global.expect = chai.expect
 global.sinon = sinon
-chai.use(sinonChai)
