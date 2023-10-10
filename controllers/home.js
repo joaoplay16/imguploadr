ae22c2586ac3c52b84ce19bc428ce2c6b662b215
João Pedro <joaoplay16@gmail.com>
2023-10-09 22:42:45 -0300

    Adapt to the sidebar function that returns a promise

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,5 @@
 var sidebar = require("../helpers/sidebar"),
-ImageModel = require("../models").Image
+  ImageModel = require("../models").Image
 
 module.exports = {
   index: function (req, res) {
@@ -7,17 +7,16 @@ module.exports = {
       images: [],
     }
 
-    ImageModel.find(
-      {},
-      {},
-      { sort: { timestamp: -1 } },
-    ).exec().then(images => {
-      viewModel.images = images
-      sidebar(viewModel, function (viewModel) {
-        res.render("index", viewModel)
+    ImageModel.find({}, {}, { sort: { timestamp: -1 } })
+      .exec()
+      .then((images) => {
+        viewModel.images = images
+        sidebar(viewModel).then((vm) => {
+          res.render("index", vm)
+        })
       })
-    }).catch(err => {
-      throw err
-    })
+      .catch((err) => {
+        throw err
+      })
   },
 }
