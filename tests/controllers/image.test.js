3f97e12e14300791dcfd0652940664d02430c8ae
João Pedro <joaoplay16@gmail.com>
2023-10-09 23:23:05 -0300

    Rollback to the async await approach
    The "done" approach is being missused e causing false positives.
    
    Also make the `sidebarStub` return a resolved promise in order to fulfill the tests requirements, once currently only returns a plain function.

--- tests/controllers/image.test.js
+++ tests/controllers/image.test.js
@@ -1,12 +1,15 @@
 var proxyquire = require("proxyquire"),
   callback = sinon.spy(),
-  sidebarStub = sinon.stub(),
+  sidebarStub = sinon
+    .stub()
+    .returns(sinon.promise().resolve({ image: {}, comments: [] })),
   fsStub = {},
   pathStub = {},
   md5Stub = {},
   ModelsStub = {
     Image: {
       findOne: sinon.stub().returns({ exec: () => sinon.promise() }),
+      save: sinon.stub(),
     },
     Comment: {
       find: sinon.stub().returns({ exec: () => sinon.promise() }),
@@ -52,6 +55,7 @@ describe("Image Controller", function () {
       },
     ]
   })
+
   describe("Index", function () {
     it("should be defined", function () {
       expect(image.index).to.exist
@@ -83,60 +87,75 @@ describe("Image Controller", function () {
             }
           )
           .returns({
-            exec: () =>
-              sinon.promise().resolve({
-                testComents,
-              }),
+            exec: () => sinon.promise().resolve(testComents),
           })
       })
-      it("should incremement views by 1 and save", function (done) {
-        image.index(req, res)
-        done()
+      it("should incremement views by 1 and save", async function () {
+        await image.index(req, res)
         expect(testImage.views).to.equal(1)
         expect(testImage.save).to.be.called
       })
-      it("should find related comments", function (done) {
-        image.index(req, res)
-        done()
+      it("should find related comments", async function () {
+        await image.index(req, res)
+
         expect(ModelsStub.Comment.find).to.be.calledWith(
           { image_id: 1 },
           {},
           { sort: { timestamp: 1 } }
         )
       })
-      it("should execute sidebar", function (done) {
-        image.index(req, res)
-        done()
+      it("should execute sidebar", async function () {
+        await image.index(req, res)
+
         expect(ModelsStub.Comment.find).to.be.called
         expect(sidebarStub).to.be.calledWith({
           image: testImage,
           comments: testComents,
         })
       })
-      it("should render image template with image and comments", function (done) {
-        image.index(req, res)
-
+      it("should render image template with image and comments", async function () {
+        await image.index(req, res)
         // use `done` to tell to the test to wait all async operations finish before
         // do the assertions
-        done()
-        expect(sidebarStub).to.be.calledWith({
-          image: testImage,
-          comments: testComents,
-        })
+        expect(sidebarStub).to.be.called
 
-        expect(res.render).to.be.calledWith("image", testComents)
+        expect(res.render).to.be.calledWith("image", {
+          image: sinon.match.object,
+          comments: sinon.match.array,
+        })
       })
 
-      it("should redirect to '/' when image is not found", function (done) {
-        ModelsStub.Image.find = sinon
+      it("should redirect to '/' when image is not found", async function () {
+        ModelsStub.Image.findOne = sinon
           .stub()
-          .returns(sinon.promise().resolve(null))
+          .returns({ exec: () => sinon.promise().resolve(null) })
 
-        image.index(req, res)
-        done()
+        await image.index(req, res)
 
         expect(res.redirect).to.be.calledWith("/")
       })
     })
   })
+
+  // describe("Create", function () {
+  //   describe("with uploaded image data", function () {
+  //     beforeEach(function () {
+  //       ModelsStub.Image.find = sinon
+  //         .stub()
+  //         .returns({ exec: () => sinon.promise().resolve([]) })
+
+  //       req.file = {
+  //         originalName: "image.png",
+  //         path: "/home/user/images",
+  //         title: "jaberwok",
+  //         description: "jaberwok is a poem",
+  //       }
+  //     })
+  //     it("should create and save an image", function (done) {
+  //       image.create(req, res)
+  //       expect(res.redirect).to.be.called
+  //       done()
+  //     })
+  //   })
+  // })
 })
