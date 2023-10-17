d49c8ec417278adb3e616851b2ebb5f0574ba25d
João Pedro <joaoplay16@gmail.com>
2023-10-16 23:43:18 -0300

    Migrate models to typescript

---
+++ models/comment.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from "mongoose"
+import { IImage } from "./image"
+
+export interface IComment extends Document {
+  image_id: Schema.Types.ObjectId
+  email: string
+  name: string
+  gravatar: string
+  comment: string
+  timestamp: Date
+  _image?: IImage
+}
+
+const CommentSchema = new Schema<IComment>({
+  image_id: { type: Schema.Types.ObjectId },
+  email: { type: String },
+  name: { type: String },
+  gravatar: { type: String },
+  comment: { type: String },
+  timestamp: { type: Date, default: Date.now },
+})
+
+CommentSchema.virtual("image")
+  .set(function (this: IComment, image: IImage) {
+    this._image = image
+  })
+  .get(function (this: IComment) {
+    return this._image
+  })
+
+export default mongoose.model<IComment>("Comment", CommentSchema)
---
+++ models/image.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document } from "mongoose"
+import path from "path"
+
+export interface IImage extends Document {
+  title: string
+  description: string
+  filename: string
+  views: number
+  likes: number
+  timestamp: Date
+  uniqueId: string
+}
+
+const ImageSchema = new Schema<IImage>({
+  title: { type: String },
+  description: { type: String },
+  filename: { type: String },
+  views: { type: Number, default: 0 },
+  likes: { type: Number, default: 0 },
+  timestamp: { type: Date, default: Date.now },
+})
+
+ImageSchema.virtual("uniqueId").get(function (this: IImage) {
+  return this.filename.replace(path.extname(this.filename), "")
+})
+
+export default mongoose.model<IImage>("Image", ImageSchema)
